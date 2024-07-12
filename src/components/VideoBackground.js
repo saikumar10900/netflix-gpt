56a31f1b1ac8a96a92bbd8f02ea6191d503f9f1b
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-07-12 18:08:34 +0530

    code refactor

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
-import { options } from "../utils/constants";
-import { addTrailerVideo } from "../utils/movieSlice";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
